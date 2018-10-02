e611717c7a663adfbc2b85526ea4918e9a440749
Felipe Arraño <farranoa@everis.com>
2018-10-02 14:55:51 -0300

    Primer commit

---
+++ src/main/java/com/in28minutes/springboot/soap/web/services/example/springboottutorialsoapwebservices/SpringBootTutorialSoapWebServicesApplication.java
@@ -0,0 +1,12 @@
+package com.in28minutes.springboot.soap.web.services.example.springboottutorialsoapwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutorialSoapWebServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTutorialSoapWebServicesApplication.class, args);
+    }
+}
