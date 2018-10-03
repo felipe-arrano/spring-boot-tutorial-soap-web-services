714c74a7a76b23236f941b6552d4483b129eec29
Felipe Arraño <farranoa@everis.com>
2018-10-03 14:15:42 -0300

    Terminando...

---
+++ src/main/java/com/in28minutes/students/GetStudentDetailsResponse.java
@@ -0,0 +1,71 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2018.10.02 a las 04:21:46 PM CLST 
+//
+
+
+package com.in28minutes.students;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="StudentDetails" type="{http://in28minutes.com/students}StudentDetails"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "studentDetails"
+})
+@XmlRootElement(name = "GetStudentDetailsResponse")
+public class GetStudentDetailsResponse {
+
+    @XmlElement(name = "StudentDetails", required = true)
+    protected StudentDetails studentDetails;
+
+    /**
+     * Obtiene el valor de la propiedad studentDetails.
+     * 
+     * @return
+     *     possible object is
+     *     {@link StudentDetails }
+     *     
+     */
+    public StudentDetails getStudentDetails() {
+        return studentDetails;
+    }
+
+    /**
+     * Define el valor de la propiedad studentDetails.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link StudentDetails }
+     *     
+     */
+    public void setStudentDetails(StudentDetails value) {
+        this.studentDetails = value;
+    }
+
+}
---
+++ src/main/java/com/in28minutes/students/StudentDetails.java
@@ -0,0 +1,116 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2018.10.02 a las 04:21:46 PM CLST 
+//
+
+
+package com.in28minutes.students;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para StudentDetails complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="StudentDetails">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="passportNumber" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "StudentDetails", propOrder = {
+    "id",
+    "name",
+    "passportNumber"
+})
+public class StudentDetails {
+
+    protected int id;
+    @XmlElement(required = true)
+    protected String name;
+    @XmlElement(required = true)
+    protected String passportNumber;
+
+    /**
+     * Obtiene el valor de la propiedad id.
+     * 
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Define el valor de la propiedad id.
+     * 
+     */
+    public void setId(int value) {
+        this.id = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad name.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Define el valor de la propiedad name.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad passportNumber.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+
+    /**
+     * Define el valor de la propiedad passportNumber.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPassportNumber(String value) {
+        this.passportNumber = value;
+    }
+
+}
